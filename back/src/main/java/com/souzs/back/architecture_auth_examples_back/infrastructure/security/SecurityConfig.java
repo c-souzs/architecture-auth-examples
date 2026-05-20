7a5604da6e579ed36b6402977c7c8c24ca224085
c-souzs <souzsdev@gmail.com>
2026-05-20 15:22:38 -0300

    feat(back): configuracao CORS

--- back/src/main/java/com/souzs/back/architecture_auth_examples_back/infrastructure/security/SecurityConfig.java
+++ back/src/main/java/com/souzs/back/architecture_auth_examples_back/infrastructure/security/SecurityConfig.java
@@ -16,6 +16,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors.configure(http))
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()
