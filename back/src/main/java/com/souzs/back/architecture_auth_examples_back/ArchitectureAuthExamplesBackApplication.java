dcd5b41663a44bc4621a6d0c7136ac94b197b2c7
c-souzs <souzsdev@gmail.com>
2026-05-20 13:37:37 -0300

    start(back): first commit

---
+++ back/src/main/java/com/souzs/back/architecture_auth_examples_back/ArchitectureAuthExamplesBackApplication.java
@@ -0,0 +1,13 @@
+package com.souzs.back.architecture_auth_examples_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitectureAuthExamplesBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchitectureAuthExamplesBackApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/souzs/back/architecture_auth_examples_back/ArchitectureAuthExamplesBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.souzs.back.architecture_auth_examples_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitectureAuthExamplesBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
