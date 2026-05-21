deb6c97afb0a1954f584716ed34e0af672117305
c-souzs <souzsdev@gmail.com>
2026-05-21 16:56:56 -0300

    feat(back): impl auth controller

---
+++ back/src/main/java/com/souzs/back/architecture_auth_examples_back/domain/auth/controller/AuthController.java
@@ -0,0 +1,48 @@
+package com.souzs.back.architecture_auth_examples_back.domain.auth.controller;
+
+import com.souzs.back.architecture_auth_examples_back.domain.auth.dto.*;
+import com.souzs.back.architecture_auth_examples_back.domain.auth.service.AuthService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<LoginResponse> register(
+            @Valid @RequestBody RegisterRequest request,
+            HttpServletResponse response) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(authService.register(request, response));
+    }
+
+    @PostMapping("/login")
+    public LoginResponse login(
+            @Valid @RequestBody LoginRequest request,
+            HttpServletResponse response) {
+        return authService.login(request, response);
+    }
+
+    @PostMapping("/refresh")
+    public RefreshResponse refresh(
+            HttpServletRequest request,
+            HttpServletResponse response) {
+        return authService.refresh(request, response);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(
+            HttpServletRequest request,
+            HttpServletResponse response) {
+        authService.logout(request, response);
+        return ResponseEntity.noContent().build();
+    }
+}
