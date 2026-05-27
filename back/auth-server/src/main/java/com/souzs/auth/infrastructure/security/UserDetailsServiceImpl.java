187af685c2c8e86664b108e2c37c06c753cc6fa8
c-souzs <souzsdev@gmail.com>
2026-05-27 13:49:32 -0300

    feat(back): impl do UserDetailsService

---
+++ back/auth-server/src/main/java/com/souzs/auth/infrastructure/security/UserDetailsServiceImpl.java
@@ -0,0 +1,34 @@
+package com.souzs.auth.infrastructure.security;
+
+import com.souzs.auth.domain.entity.User;
+import com.souzs.auth.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .authorities(List.of())
+                .disabled(!user.isEnabled())
+                .accountLocked(user.isLocked())
+                .build();
+    }
+}
